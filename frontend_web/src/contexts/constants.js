bb3b56fb5fc1976f8308f683e3f5f5cbb87c2c72
Tranguyen1610 <thnguyen.7620@gmail.com>
2023-05-31 21:16:00 +0700

    fix delete

--- frontend_web/src/contexts/constants.js
+++ frontend_web/src/contexts/constants.js
@@ -1,4 +1,4 @@
-export const apiUrl = "http://192.168.1.11:5000/api/user";
-// export const Url = 'https://n2app.onrender.com'
-export const Url = "http://localhost:5000";
+// export const apiUrl = "http://192.168.1.11:5000/api/user";
+export const Url = 'https://n2app.onrender.com'
+// export const Url = "http://localhost:5000";
 export const UrlSK = "ws://18.142.139.131:8800";
